54724bef7bbd42f54a7440c7a3bef20629c256d8
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-09 18:31:56 +0200

    added animations in hero

--- components/Home/Hero/Hero.jsx
+++ components/Home/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import LinkButton from '../../UI/LinkButton';
 import Blob from './Blob';
 import classes from './Hero.module.css';
-
+import { Typewriter } from 'react-simple-typewriter';
 const Hero = () => {
 	return (
 		<section className={classes.hero}>
@@ -10,9 +10,19 @@ const Hero = () => {
 				<Blob />
 				<div className={classes.textContainer}>
 					<h1>
-						Hi, im <span>Oskar</span>
+						Hi, I'm <span className={classes.headName}>Oskar</span>
 					</h1>
-					<p>Im frontend Developer</p>
+					<p>
+						I am
+						<Typewriter
+							loop={10}
+							typeSpeed={100}
+							delaySpeed={2000}
+							cursor
+							cursorStyle="|"
+							words={[' Web Designer', ' Web Developer', ' Frontend Developer']}
+						/>
+					</p>
 				</div>
 				<div className={classes.links}>
 					<LinkButton href="/contact">Contact</LinkButton>
