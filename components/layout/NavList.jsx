98124f4bfe076746da9b8459bc9dd49e580dfda4
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-10 18:21:03 +0200

    fixed navbar classes adding between sizes of screen

--- components/layout/NavList.jsx
+++ components/layout/NavList.jsx
@@ -1,21 +1,33 @@
+import { useEffect, useState } from 'react';
 import { Sling as Hamburger } from 'hamburger-react';
 import classes from './NavList.module.css';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 const NavList = ({ isNavActive, mobileNavHandler }) => {
+	const [isMobile, setIsMobile] = useState(null);
 	const router = useRouter();
-
 	const links = [
 		{ href: '/', name: 'Home' },
 		{ href: '/about-me', name: 'About Me' },
 		{ href: '/projects', name: 'Projects' },
 		{ href: '/contact', name: 'Contact' },
 	];
+	const screenXHandler = () => {
+		if (window.innerWidth > 768) {
+			setIsMobile(false);
+		} else {
+			setIsMobile(true);
+		}
+	};
+	useEffect(() => {
+		window.addEventListener('resize', screenXHandler);
+		screenXHandler();
+	}, [screenXHandler]);
 
 	const linkis = links.map((link) => (
 		<li
 			key={link.name}
-			onClick={mobileNavHandler}
+			onClick={isMobile ? mobileNavHandler : () => {}}
 			className={link.href === router.pathname ? classes.activeLink : null}
 		>
 			<Link href={link.href}>{link.name}</Link>
