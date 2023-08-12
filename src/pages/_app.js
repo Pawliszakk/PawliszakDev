37172b27797c34fba14d6c79db2f645765266254
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-12 22:49:13 +0200

    added favicon and apple icon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,13 @@ export default function App({ Component, pageProps }) {
 		<>
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				
+				<link rel="icon" href="/assets/icons/favicon.ico" sizes="any" />
+				<link
+					rel="apple-touch-icon"
+					href="/assets/icons/apple-touch-icon.png"
+					type="image/png"
+					sizes="180x180"
+				/>
 			</Head>
 			<Layout>
 				<Component {...pageProps} />
