41e231693ac31933f6e6fa494be0da2576f6079d
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-16 23:12:04 +0200

    little styled decoration section

--- components/Home/Decoration/DecorationSlide.jsx
+++ components/Home/Decoration/DecorationSlide.jsx
@@ -1,22 +1,30 @@
 import classes from './DecorationSlide.module.css';
-
+import { FaMobileAlt } from 'react-icons/fa';
+import { TbWorldSearch, TbDeviceMobileHeart } from 'react-icons/tb';
 const DecorationSlide = () => {
 	return (
 		<section className={classes.decoration}>
+            <div className={`${classes.block} block`}></div>
 			<div className={classes.box}>
 				<div className={classes.pros}>
-					<h2>RWD</h2>
+					<h2>
+						<FaMobileAlt /> RWD
+					</h2>
 					<p>Design suitable for both mobile devices and desktop computers</p>
 				</div>
 				<div className={classes.pros}>
-					<h2>UI/UX</h2>
+					<h2>
+						<TbDeviceMobileHeart /> UI/UX
+					</h2>
 					<p>
 						Designed so that people with different technical backgrounds and
 						levels of knowledge can navigate it effortlessly.
 					</p>
 				</div>
 				<div className={classes.pros}>
-					<h2>SEO</h2>
+					<h2>
+						<TbWorldSearch /> SEO
+					</h2>
 					<p>
 						Creating valuable, engaging and unique content for search engines
 					</p>
