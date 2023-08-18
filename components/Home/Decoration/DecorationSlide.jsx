1a1335a6effe87ad473aeecb8fc9cd8cfd66954a
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-18 17:14:27 +0200

    styled decoration, not finished

--- components/Home/Decoration/DecorationSlide.jsx
+++ components/Home/Decoration/DecorationSlide.jsx
@@ -1,40 +1,13 @@
 import classes from './DecorationSlide.module.css';
-import { FaMobileAlt } from 'react-icons/fa';
-import { TbWorldSearch, TbDeviceMobileHeart } from 'react-icons/tb';
+
 import Pros from './Pros';
 const DecorationSlide = () => {
-	const prosData = [
-		{
-			icon: <FaMobileAlt />,
-			header: 'RWD',
-			paragraph:
-				'Developing a design that easily adapts to different screen sizes ensures an inclusive and user-friendly experience, increasing accessibility and engagement.',
-		},
-		{
-			icon: <TbDeviceMobileHeart />,
-			header: 'UI/UX',
-			paragraph:
-				'Designed so that people with different technical backgrounds and levels of knowledge can navigate it effortlessly',
-		},
-		{
-			icon: <TbWorldSearch />,
-			header: 'SEO',
-			paragraph:
-				'Creating valuable, engaging and original content for search engines requires a blend of creativity and strategic keyword optimisation.',
-		},
-	];
-	const prosComponents = prosData.map((item, index) => (
-		<Pros
-			key={index}
-			icon={item.icon}
-			header={item.header}
-			paragraph={item.paragraph}
-		/>
-	));
 	return (
 		<section className={classes.decoration}>
 			<div className={`${classes.block} block`}></div>
-			<div className={classes.box}>{prosComponents}</div>
+			<div className={classes.box}>
+				<Pros />
+			</div>
 		</section>
 	);
 };
--- components/Home/Decoration/Pros.jsx
+++ components/Home/Decoration/Pros.jsx
@@ -1,12 +1,38 @@
 import classes from './Pros.module.css';
+import { FaMobileAlt } from 'react-icons/fa';
+import { TbWorldSearch, TbDeviceMobileHeart } from 'react-icons/tb';
 const Pros = ({ header, paragraph, icon }) => {
 	return (
-		<div className={classes.pros}>
-			<h2>
-				{icon} {header}
-			</h2>
-			<p>{paragraph}</p>
-		</div>
+		<>
+			<div className={`${classes.pros} ${classes.firstPros}`}>
+				<h2>
+					<FaMobileAlt /> RWD
+				</h2>
+				<p>
+					Developing a design that easily adapts to different screen sizes
+					ensures an inclusive and user-friendly experience, increasing
+					accessibility and accessibility and engagement.
+				</p>
+			</div>
+			<div className={`${classes.pros} ${classes.secondPros}`}>
+				<h2>
+					<TbDeviceMobileHeart /> UI/UX
+				</h2>
+				<p>
+					Designed so that people with different technical backgrounds and
+					levels of knowledge can navigate it effortlessly
+				</p>
+			</div>
+			<div className={classes.pros}>
+				<h2>
+					<TbWorldSearch /> SEO
+				</h2>
+				<p>
+					Creating valuable, engaging and original content for search engines
+					requires a blend of creativity and strategic keyword optimisation.
+				</p>
+			</div>
+		</>
 	);
 };
 
