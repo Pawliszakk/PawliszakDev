869fa8e978ca7b69c91a8c157351b9ae99cac63a
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-18 19:14:23 +0200

    added block in my projects section

--- components/Home/Decoration/DecorationSlide.jsx
+++ components/Home/Decoration/DecorationSlide.jsx
@@ -4,10 +4,10 @@ import Pros from './Pros';
 const DecorationSlide = () => {
 	return (
 		<section className={classes.decoration}>
-			
 			<div className={classes.box}>
 				<Pros />
 			</div>
+			<div className={`${classes.block} block`}></div>
 		</section>
 	);
 };
