8ec530bc2c304f83b95e3209228fb273006c461a
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-20 23:17:26 +0200

    changed slightly descriptions in decoration pros

--- components/Home/Decoration/Pros.jsx
+++ components/Home/Decoration/Pros.jsx
@@ -8,24 +8,25 @@ const Pros = () => {
 				<HiOutlineDeviceMobile />
 				<h2>RWD</h2>
 				<p>
-					Developing a design that easily adapts to different screen sizes
-					ensures an inclusive and user-friendly experience, increasing
-					accessibility and accessibility and engagement.
+					Creating a design that easily adapts to different screen sizes ensures
+					an inclusive and user-friendly experience, increasing accessibility
+					and accessibility and engagement.
 				</p>
 			</div>
 			<div className={classes.pros}>
 				<TbDeviceMobileHeart />
 				<h2>UI/UX</h2>
 				<p>
-					Designed so that people with different technical backgrounds and
-					levels of knowledge can navigate it effortlessly
+					Eye-catching designs. Also designed so that people with different
+					technical backgrounds and levels of knowledge can navigate
+					effortlessly.
 				</p>
 			</div>
 			<div className={classes.pros}>
 				<TbWorldSearch />
 				<h2>SEO</h2>
 				<p>
-					Creating valuable, engaging and original content for search engines
+					Developing valuable, engaging and original content for search engines
 					requires a blend of creativity and strategic keyword optimisation.
 				</p>
 			</div>
