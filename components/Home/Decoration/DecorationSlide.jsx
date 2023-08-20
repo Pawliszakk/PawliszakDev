1fdf83fa9573fc1b014c5975ca2480cda85f8a47
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-20 22:20:00 +0200

    changed decoration pros

--- components/Home/Decoration/DecorationSlide.jsx
+++ components/Home/Decoration/DecorationSlide.jsx
@@ -1,12 +1,10 @@
 import classes from './DecorationSlide.module.css';
-import { PiGlobeThin } from 'react-icons/pi';
 
 import Pros from './Pros';
 const DecorationSlide = () => {
 	return (
 		<section className={classes.decoration}>
 			<Pros />
-			<PiGlobeThin className={classes.icon} />
 		</section>
 	);
 };
--- components/Home/Decoration/Pros.jsx
+++ components/Home/Decoration/Pros.jsx
@@ -4,30 +4,26 @@ import { TbWorldSearch, TbDeviceMobileHeart } from 'react-icons/tb';
 const Pros = () => {
 	return (
 		<div className={classes.box}>
-			
-			<div className={`${classes.pros} ${classes.firstPros}`}>
-				<h2>
-					<FaMobileAlt /> RWD
-				</h2>
+			<div className={classes.pros}>
+				<FaMobileAlt />
+				<h2>RWD</h2>
 				<p>
 					Developing a design that easily adapts to different screen sizes
 					ensures an inclusive and user-friendly experience, increasing
 					accessibility and accessibility and engagement.
 				</p>
 			</div>
-			<div className={`${classes.pros} ${classes.secondPros}`}>
-				<h2>
-					<TbDeviceMobileHeart /> UI/UX
-				</h2>
+			<div className={classes.pros}>
+				<TbDeviceMobileHeart />
+				<h2>UI/UX</h2>
 				<p>
 					Designed so that people with different technical backgrounds and
 					levels of knowledge can navigate it effortlessly
 				</p>
 			</div>
-			<div className={`${classes.pros} ${classes.thirdPros}`}>
-				<h2>
-					<TbWorldSearch /> SEO
-				</h2>
+			<div className={classes.pros}>
+				<TbWorldSearch />
+				<h2>SEO</h2>
 				<p>
 					Creating valuable, engaging and original content for search engines
 					requires a blend of creativity and strategic keyword optimisation.
