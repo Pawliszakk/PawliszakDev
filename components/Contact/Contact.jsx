cac33aa82f73c76b893ab31fecc635e045499fd0
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-21 12:51:52 +0200

    commented contact logic start

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -4,16 +4,16 @@ import Modal from '../UI/Modal';
 import classes from './Contact.module.css';
 
 const Contact = () => {
-	const [isModal, setIsModal] = useState(true);
+	const [isModal, setIsModal] = useState(false);
 
 	const hideModalHandler = () => setIsModal(false);
 	const showModalHandler = () => setIsModal(true);
 	return (
 		<section className={classes.contact}>
 			<h1>Strona kontakt jest w trakcie budowy...</h1>
-
+{/* 
 			<button onClick={showModalHandler}>pokaz modal</button>
-			<button onClick={hideModalHandler}>schowaj modal</button>
+			<button onClick={hideModalHandler}>schowaj modal</button> */}
 
 			{isModal ? (
 				<Modal show={isModal} onClose={hideModalHandler}>
