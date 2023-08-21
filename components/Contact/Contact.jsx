ac4c7914ef637d67837d43bae4544e2ea7603872
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-21 19:13:30 +0200

    added portal function in the lib folder and cleared a bit modal component, added timeout on closing the modal in contact

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Modal from '../UI/Modal';
 import classes from './Contact.module.css';
@@ -8,18 +8,22 @@ const Contact = () => {
 
 	const hideModalHandler = () => setIsModal(false);
 	const showModalHandler = () => setIsModal(true);
+	useEffect(() => {
+		const modalTimer = setTimeout(() => setIsModal(false), 3000);
+		return () => clearInterval(modalTimer);
+	}, [isModal]);
+
+	
 	return (
 		<section className={classes.contact}>
 			<h1>Strona kontakt jest w trakcie budowy...</h1>
-{/* 
-			<button onClick={showModalHandler}>pokaz modal</button>
-			<button onClick={hideModalHandler}>schowaj modal</button> */}
 
-			{isModal ? (
-				<Modal show={isModal} onClose={hideModalHandler}>
-					Copied to clipboard!
-				</Modal>
-			) : null}
+			<button onClick={showModalHandler}>SHOW MODAL</button>
+			<button onClick={hideModalHandler}>CLOSE MODAL</button>
+
+			<Modal show={isModal} onClose={hideModalHandler}>
+				Copied to clipboard!
+			</Modal>
 		</section>
 	);
 };
--- components/UI/Modal.jsx
+++ components/UI/Modal.jsx
@@ -1,27 +1,31 @@
-import { useEffect, useState } from 'react';
-import { createPortal } from 'react-dom';
-
 import classes from './Modal.module.css';
 import { BsClipboardCheck } from 'react-icons/bs';
+import { Transition } from 'react-transition-group';
+import Portal from '../../src/lib/Portal';
 
 const Modal = ({ children, onClose, show }) => {
-	const [mounted, setMounted] = useState(false);
-
-	useEffect(() => {
-		setMounted(true);
-
-		return () => setMounted(false);
-	}, []);
-	const content = (
-		<div onClick={onClose} className={classes.modal}>
-			<BsClipboardCheck />
-			<h2>{children}</h2>
-		</div>
+	return (
+		<Portal>
+			<Transition in={show} timeout={0}>
+				{(state) => (
+					<div
+						onClick={onClose}
+						className={classes.modal}
+						style={{
+							transition: 'transform 0.3s ease-out',
+							transform:
+								state === 'exited'
+									? 'translate(-50%, -150px)'
+									: 'translate(-50%,0)',
+						}}
+					>
+						<BsClipboardCheck />
+						<h2>{children}</h2>
+					</div>
+				)}
+			</Transition>
+		</Portal>
 	);
-
-	return mounted
-		? createPortal(content, document.getElementById('modal'))
-		: null;
 };
 
 export default Modal;
