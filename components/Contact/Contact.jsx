3438b69f9d849ed6f9dfd9a37b76b7c83ef299a3
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-22 19:48:28 +0200

    optimized backdrop and modal in contact

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
-
+import { BsClipboardCheck } from 'react-icons/bs';
 import classes from './Contact.module.css';
 import ContactOptions from './ContactOptions/ContactOptions';
 import AvatarBlob from '../UI/AvatarBlob';
 import SectionBlock from '../UI/SectionBlock';
 import Modal from '../UI/PortalComponents/Modal';
-import Backdrop from '../UI/PortalComponents/Backdrop';
 
 const Contact = () => {
 	const [isModal, setIsModal] = useState(false);
 
 	const hideModalHandler = () => setIsModal(false);
 	const showModalHandler = () => setIsModal(true);
+
 	useEffect(() => {
 		const modalTimer = setTimeout(() => setIsModal(false), 3000);
 		return () => clearInterval(modalTimer);
@@ -27,14 +27,15 @@ const Contact = () => {
 			/>
 			<h1>Get in touch with me!</h1>
 			<ContactOptions showModal={showModalHandler} />
-			
+
 			{isModal && (
-				<>
-					<Modal show={isModal} onClose={hideModalHandler}>
-						Copied to clipboard!
-					</Modal>
-					<Backdrop show={isModal} onClose={hideModalHandler} />
-				</>
+				<Modal
+					show={isModal}
+					onClose={hideModalHandler}
+					icon={<BsClipboardCheck />}
+				>
+					Copied to clipboard!
+				</Modal>
 			)}
 			<SectionBlock left footer />
 		</section>
--- components/UI/PortalComponents/Modal.jsx
+++ components/UI/PortalComponents/Modal.jsx
@@ -1,10 +1,10 @@
 import classes from './Modal.module.css';
-import { BsClipboardCheck } from 'react-icons/bs';
+
 import { motion, AnimatePresence } from 'framer-motion';
 import Portal from '@/lib/Portal';
 import Backdrop from './Backdrop';
 
-const Modal = ({ children, onClose, show }) => {
+const Modal = ({ children, onClose, show, icon }) => {
 	const dropIn = {
 		hidden: {
 			top: '-5rem',
@@ -23,18 +23,21 @@ const Modal = ({ children, onClose, show }) => {
 	return (
 		<AnimatePresence>
 			{show && (
-				<Portal>
-					<motion.div
-						className={classes.modal}
-						variants={dropIn}
-						initial="hidden"
-						animate="visible"
-						onClick={onClose}
-					>
-						<BsClipboardCheck />
-						<h2>{children}</h2>
-					</motion.div>
-				</Portal>
+				<>
+					<Portal>
+						<motion.div
+							className={classes.modal}
+							variants={dropIn}
+							initial="hidden"
+							animate="visible"
+							onClick={onClose}
+						>
+							{icon}
+							<h2>{children}</h2>
+						</motion.div>
+					</Portal>
+					<Backdrop onClose={onClose} show={show} />
+				</>
 			)}
 		</AnimatePresence>
 	);
