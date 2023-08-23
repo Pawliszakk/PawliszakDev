edf7f1500f6148812304d21d04264ddd28cbd361
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-23 18:18:27 +0200

    turned back description

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,14 +8,11 @@ const HomePage = () => {
 		<>
 			<Head>
 				<title>PawliszakDev - Home Page</title>
-				{/* <meta
-					name="description"
-					content="The portfolio website of React Frontend Developer, Oskar Pawliszak. Check out my latest projects and learn more about me and my skills."
-				/> */}
 				<meta
 					name="description"
-					content="I'm Oskar Pawliszak, a React Frontend Developer. Welcome to my portfolio website, PawliszakDev, where you can explore my latest projects and learn more about my skills and experiences. Feel free to browse through my work and get to know me better!"
+					content="The portfolio website of React Frontend Developer, Oskar Pawliszak. Check out my latest projects and learn more about me and my skills."
 				/>
+
 			</Head>
 			<Hero />
 			<AboutMe />
