512061931ab4ea9dbe8c1a13e7594526c6efca75
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-26 23:28:08 +0200

    added projects map in projectDemos

--- components/Home/Projects/ProjectDemos.jsx
+++ components/Home/Projects/ProjectDemos.jsx
@@ -1,19 +1,24 @@
 import classes from './ProjectDemos.module.css';
 import ProjectDemo from './ProjectDemo';
 const ProjectDemos = () => {
+	const projects = [
+		{
+			href: 'https://klimaswiat.pl/',
+			src: '/assets/projects/klimaswiat.JPG',
+			alt: 'Klima Świat Home page snippet',
+		},
+		{
+			src: '/assets/projects/ecofresh.JPG',
+			alt: 'Eco fresh Home page snippet',
+			href: 'https://www.hurtowniaecofresh.pl/',
+		},
+	];
+
 	return (
 		<div className={classes.box}>
-			<ProjectDemo
-				href="https://klimaswiat.pl/"
-				src="/assets/projects/klimaswiat.JPG"
-				alt="Klima Świat Home page snippet"
-			/>
-
-			<ProjectDemo
-				src="/assets/projects/ecofresh.JPG"
-				alt="Eco fresh Home page snippet"
-				href="https://www.hurtowniaecofresh.pl/"
-			/>
+			{projects.map((project) => (
+				<ProjectDemo href={project.href} src={project.src} alt={project.alt} />
+			))}
 		</div>
 	);
 };
