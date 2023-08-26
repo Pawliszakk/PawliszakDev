efb779ed2c241a24d817ad35ab637cfba2b73b16
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-27 01:43:45 +0200

    added paragraph in latest projects

--- components/Home/Projects/Projects.jsx
+++ components/Home/Projects/Projects.jsx
@@ -1,18 +1,26 @@
 import SectionBlock from '../../UI/SectionBlock';
 import SectionTitle from '../../UI/SectionTItle';
 import LinkButton from '../../UI/buttons/LinkButton';
+import SpecialLink from '../../UI/buttons/SpecialLink';
 import ProjectDemos from './ProjectDemos';
 import classes from './Projects.module.css';
 
 const Projects = () => {
 	return (
 		<section className={classes.projects}>
-			<SectionBlock className={classes.block} left />
 			<SectionTitle>Latest Projects</SectionTitle>
 
-			<ProjectDemos />
-
-			<LinkButton href="/projects">Projects</LinkButton>
+			<div className={classes.container}>
+				<ProjectDemos />
+				<p>
+					If you want to dig deeper into my code or see more projects make sure
+					to visit my{' '}
+					<SpecialLink href="https://github.com/Pawliszakk">GitHub</SpecialLink>{' '}
+					profile or click the <span>Projects</span> button below{' '}
+				</p>
+				<LinkButton href="/projects">Projects</LinkButton>
+			</div>
+			<SectionBlock className={classes.block} left />
 			<SectionBlock left footer />
 		</section>
 	);
