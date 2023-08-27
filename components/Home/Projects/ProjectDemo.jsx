4845fbf1068dc3d7f635e20538cb9848b72bc92d
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-27 23:12:45 +0200

    fixed lagging projects animation

--- components/Home/Projects/ProjectDemo.jsx
+++ components/Home/Projects/ProjectDemo.jsx
@@ -7,21 +7,25 @@ const ProjectDemo = ({ href, src, alt, title, tech }) => {
 	const techMap = tech.map((tech) => <li key={tech}>{tech}</li>);
 
 	return (
-		<div className={classes.card}>
-			<div className={classes.image}>
-				<Image src={src} alt={alt} fill sizes="100%" />
-			</div>
-			<div className={classes.text}>
-				<div>
-					<h3>{title}</h3>
-					<p>Technologies</p>
-					<ul>{techMap}</ul>
+		<div className={classes.box}>
+			<div className={classes.card}>
+				<div className={classes.image}>
+					<Image src={src} alt={alt} fill sizes="100%" />
 				</div>
-				<div className={classes.buttons}>
-					<LinkButton href="/projects">Learn more about the project</LinkButton>
-					<LinkButton anchor href={href}>
-						Visit Live
-					</LinkButton>
+				<div className={classes.text}>
+					<div>
+						<h3>{title}</h3>
+						<p>Technologies</p>
+						<ul>{techMap}</ul>
+					</div>
+					<div className={classes.buttons}>
+						<LinkButton href="/projects">
+							Learn more about the project
+						</LinkButton>
+						<LinkButton anchor href={href}>
+							Visit Live
+						</LinkButton>
+					</div>
 				</div>
 			</div>
 		</div>
