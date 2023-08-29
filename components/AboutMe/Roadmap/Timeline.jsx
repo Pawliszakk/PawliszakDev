1da8070e22b4433ba9be612c60dff434f38eefb2
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-29 14:25:06 +0200

    added little styling and animation to roadmap

--- components/AboutMe/Roadmap/Timeline.jsx
+++ components/AboutMe/Roadmap/Timeline.jsx
@@ -1,7 +1,17 @@
 import classes from './Timeline.module.css';
+import { motion } from 'framer-motion';
 
 const Timeline = ({ stopsList }) => {
-	const stops = stopsList.map((stop, i) => <li key={i}>{stop.name}</li>);
+	const stops = stopsList.map((stop, i) => (
+		<motion.li
+			initial={{ opacity: 0, translateX: i % 2 == 0 ? -50 : 50 }}
+			whileInView={{ opacity: 1, translateX: i % 2 == 0 ? -20 : 20 }}
+			transition={{ delay: i * 0.4 + 0.3 }}
+			key={i}
+		>
+			{stop.name}
+		</motion.li>
+	));
 
 	return (
 		<div className={classes.timeline}>
