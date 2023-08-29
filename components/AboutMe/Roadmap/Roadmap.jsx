6b08e5e068e658cab3968e5499ac911998e86c7d
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-29 16:22:58 +0200

    added texts to some of the stops

--- components/AboutMe/Roadmap/Roadmap.jsx
+++ components/AboutMe/Roadmap/Roadmap.jsx
@@ -20,7 +20,7 @@ const Roadmap = () => {
 					<AiFillHtml5 /> <FaCss3Alt />
 				</>
 			),
-			text: 'In my first 2-3 months of learning Frontend I learned HTML5 semantics, metatags, building website skeletons out of HTML elements. What goes behind html, I learned CSS3, such topics as specifity in css, flexbox, grid, pseudoclasses, pseudoelements etc.  ',
+			text: 'In my first 2-3 months of learning Frontend I learned HTML5 semantics, metatags, building website skeletons out of HTML elements. What goes behind html, I learned CSS3, such topics as Responsive Web Design, specifity in css, flexbox, grid, pseudoclasses, pseudoelements etc.  ',
 		},
 		{
 			name: 'SASS/SCSS',
@@ -30,17 +30,17 @@ const Roadmap = () => {
 		{
 			name: 'JS ES6+',
 			icons: <BiLogoJavascript />,
-			text: 'The next stage  was JavaScript. I started by understanding the fundamentals of JavaScript, such as variables, data types, operators, and control structures. Also I learned ES6+ featured such as const, let, arrow functions, template literals, Array and object destructing, import, export etc.',
+			text: 'The next stage  was JavaScript. I started by understanding the fundamentals of JavaScript, such as variables, data types, operators, connecting to API and control structures. Also I learned ES6+ featured such as const, let, arrow functions, template literals, Array and object destructing, import, export etc.',
 		},
 		{
 			name: 'REACT.JS',
 			icons: <FaReact />,
-			text: 'The next milestone of my learning journey was React.js. I delved into the captivating realm of React.js . At its core, I learned that React revolves around components, . With React, I learned to create dynamic and interactive interfaces by composing these components together.  ',
+			text: 'The next milestone of my learning journey was React.js. I delved into the captivating realm of React.js . At its core, I learned that React revolves around components. I learned features such as props, renderig lists, conditional content, css modules, state handling, component lifecycle, connecting to Database, managing app wide state with Context, Redux, React Router, working with controlled and uncontrolled forms, and animating React components with framer motion or React Transition Group.',
 		},
 		{
 			name: 'NEXT.JS',
 			icons: <SiNextdotjs />,
-			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur voluptas illo et amet voluptates magnam, incidunt doloribus unde non possimus fugit numquam tempora, cumque reprehenderit maxime, quaerat nemo magni enim.',
+			text: 'What comes after React.js is his framework - Next.js. At this stage i started exploring the beauty of this technology such as File-based Routing, Page pre-rendering. Data fectching with SSR, SSG (with ISR). Also explored such great feature as API Routes, and working with HTTP request inside of them, and connecting to Database.',
 		},
 		{
 			name: 'MERN STACK',
--- components/AboutMe/Roadmap/Stop.jsx
+++ components/AboutMe/Roadmap/Stop.jsx
@@ -5,18 +5,21 @@ const Stop = ({ index, name, icons, text }) => {
 	const isEven = index % 2 === 0;
 
 	return (
-		<motion.li
-			initial={{ opacity: 0, translateX: isEven ? -50 : 50 }}
-			whileInView={{ opacity: 1, translateX: isEven ? -20 : 20 }}
-			transition={{ delay: index * 0.4 + 0.3 }}
-			className={classes.stop}
-		>
-			<p>{name}</p>
-			<div className={classes.icons}>{icons}</div>
-			<div className={classes.text}>
-				<p>{text}</p>
-			</div>
-		</motion.li>
+		<>
+			
+			<motion.li
+				initial={{ opacity: 0, translateX: isEven ? -50 : 50 }}
+				whileInView={{ opacity: 1, translateX: isEven ? -20 : 20 }}
+				transition={{ delay: index * 0.4 + 0.3 }}
+				className={classes.stop}
+			>
+				<p>{name}</p>
+				<div className={classes.icons}>{icons}</div>
+				<div className={classes.text}>
+					<p>{text}</p>
+				</div>
+			</motion.li>
+		</>
 	);
 };
 
