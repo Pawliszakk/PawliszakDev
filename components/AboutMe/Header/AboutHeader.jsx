74f13ac439a796f61d932e2fe68fb3d151a0f2bf
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-30 23:55:59 +0200

    changed check roadmap link to a with framer motion with arrow

--- components/AboutMe/Header/AboutHeader.jsx
+++ components/AboutMe/Header/AboutHeader.jsx
@@ -2,10 +2,12 @@ import AvatarBlob from '../../UI/AvatarBlob';
 import SectionBlock from '../../UI/SectionBlock';
 
 import SlideAnimation from '../../UI/SlideAnimation';
-import LinkButton from '../../UI/buttons/LinkButton';
+
 import classes from './AboutHeader.module.css';
 import AboutNote from './AboutNote';
 import { useTranslation } from 'react-i18next';
+import { BsFillArrowDownCircleFill } from 'react-icons/bs';
+import { motion } from 'framer-motion';
 
 const AboutHeader = () => {
 	const [t, i18n] = useTranslation('global');
@@ -25,9 +27,16 @@ const AboutHeader = () => {
 				<AboutNote heading={t('about.header')} />
 			</div>
 
-			<LinkButton href="#roadmap" className={classes.btn}>
-				{t('about.checkroadmap')}
-			</LinkButton>
+			<motion.a
+				initial={{ y: 120, opacity: 0 }}
+				whileHover={{ scale: 0.9 }}
+				whileTap={{ scale: 0.5 }}
+				whileInView={{ y: 0, opacity: 1 }}
+				href="#roadmap"
+				className={classes.btn}
+			>
+				<BsFillArrowDownCircleFill />
+			</motion.a>
 			<SectionBlock left className={classes.block} />
 		</header>
 	);
