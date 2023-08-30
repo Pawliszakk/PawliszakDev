01c03e3da547205d976a1c29ddc3e0117dd23c14
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-30 12:24:03 +0200

    changed section title to html elements design

--- components/UI/SectionTItle.jsx
+++ components/UI/SectionTItle.jsx
@@ -1,7 +1,9 @@
 import classes from './SectionTitle.module.css';
 
 const SectionTitle = ({ children, className }) => {
-	return <h2 className={`${classes.heading} ${className}`}>{children}</h2>;
+	return (
+		<h2 className={`${classes.heading} ${className}`}>&lt;{children}/&gt;</h2>
+	);
 };
 
 export default SectionTitle;
