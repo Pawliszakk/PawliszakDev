b5ad5d5920c280e4d4ade5aeb7b078e320f710bf
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-30 23:12:33 +0200

    deleted usedisplay, and conditional content in icons in contact about me

--- components/AboutMe/Contact/Icons.jsx
+++ components/AboutMe/Contact/Icons.jsx
@@ -1,40 +1,18 @@
 import { MouseParallax } from 'react-just-parallax';
 import { AiOutlineMail, AiOutlineMessage } from 'react-icons/ai';
 import classes from './Icons.module.css';
-import useDisplay from '@/hooks/useDisplay';
 
 const Icons = () => {
-	const isMobile = useDisplay();
 	return (
 		<>
-			{!isMobile && (
-				<>
-					<MouseParallax
-						enableOnTouchDevice
-						isAbsolutelyPositioned
-						strength={0.14}
-					>
-						<AiOutlineMail className={classes.mail} />
-						<AiOutlineMessage className={classes.msg} />
-					</MouseParallax>
-					<MouseParallax
-						enableOnTouchDevice
-						isAbsolutelyPositioned
-						strength={0.25}
-					>
-						<AiOutlineMail className={classes.msgShadow} />
-						<AiOutlineMessage className={classes.mailShadow} />
-					</MouseParallax>
-				</>
-			)}
-			{isMobile && (
-				<>
-					<AiOutlineMail className={classes.mail} />
-					<AiOutlineMessage className={classes.msg} />
-					<AiOutlineMail className={classes.msgShadow} />
-					<AiOutlineMessage className={classes.mailShadow} />
-				</>
-			)}
+			<MouseParallax isAbsolutelyPositioned strength={0.14}>
+				<AiOutlineMail className={classes.mail} />
+				<AiOutlineMessage className={classes.msg} />
+			</MouseParallax>
+			<MouseParallax isAbsolutelyPositioned strength={0.25}>
+				<AiOutlineMail className={classes.msgShadow} />
+				<AiOutlineMessage className={classes.mailShadow} />
+			</MouseParallax>
 		</>
 	);
 };
