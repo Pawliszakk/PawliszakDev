9b92bc3d389afacdabd2c083fb3da3e5435aa2e5
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-31 21:40:33 +0200

    added description in english to about me note

--- components/AboutMe/Header/AboutNote.jsx
+++ components/AboutMe/Header/AboutNote.jsx
@@ -9,7 +9,33 @@ const AboutNote = ({ heading }) => {
 			<SectionTitle className={classes.heading}>{heading}</SectionTitle>
 
 			<p>
-				With a keen eye for design and user experience, I specialize in
+				Hello! I'm Oskar Pawliszak, a passionate React front-end developer from
+				Zamość, Poland.{' '}
+			</p>
+			<p>
+				I've found my passion in web development. I love spending time coding
+				and facing new challenges. I've spent countless hours on frontend
+				development in the last few months, as my Github profile shows.
+			</p>
+			<p>
+				I am also a graduate of a technical school for computer science, which
+				infected me with a passion for programming. As a graduate, I have a
+				background in computer hardware and the construction and operation of
+				Internet networks.
+			</p>
+			<p>
+				Outside of programming, I am also involved in fitness in my spare time.
+				I am a personal trainer and have been passionate about sport since I was
+				a child. Find out more about me by scrolling down this page.
+				<ImQuotesLeft className={classes.quoteLeft} />
+				<ImQuotesRight className={classes.quoteRight} />
+			</p>
+		</SlideAnimation>
+	);
+};
+
+export default AboutNote;
+/*With a keen eye for design and user experience, I specialize in
 				translating concepts into intuitive and visually appealing interfaces.
 				My proficiency extends beyond React, encompassing state management
 				libraries like Redux, and a strong grasp of RESTful APIs for seamless
@@ -19,12 +45,4 @@ const AboutNote = ({ heading }) => {
 				trends and best practices, enabling me to deliver modern and
 				cutting-edge solutions. Whether it's optimizing performance or crafting
 				pixel-perfect layouts, I'm committed to delivering high-quality code
-				that elevates the digital experience.
-				<ImQuotesLeft className={classes.quoteLeft} />
-				<ImQuotesRight className={classes.quoteRight} />
-			</p>
-		</SlideAnimation>
-	);
-};
-
-export default AboutNote;
+				that elevates the digital experience. */
