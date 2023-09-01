33089bf0abaabd99e8f2637e6d93268a43202f9b
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-09-01 18:41:21 +0200

    refactored stop.jsx code, removed state, did it in vanilla css

--- components/AboutMe/Roadmap/Stop.jsx
+++ components/AboutMe/Roadmap/Stop.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react';
-
 import classes from './Stop.module.css';
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { AiOutlineInfoCircle } from 'react-icons/ai';
 const Stop = ({ index, name, icons, text }) => {
-	const [isText, setIsText] = useState(false);
-
 	const isEven = index % 2 === 0;
 
 	return (
@@ -14,25 +10,14 @@ const Stop = ({ index, name, icons, text }) => {
 			whileInView={{ opacity: 1, x: isEven ? -20 : 20 }}
 			transition={{ delay: index === 0 ? 0.3 : 0.3 + index * 0.1 }}
 			className={classes.stop}
-			onMouseEnter={() => setIsText(true)}
-			onMouseLeave={() => setIsText(false)}
 		>
 			<p>{name}</p>
 			<div className={classes.icons}>{icons}</div>
 			<AiOutlineInfoCircle className={classes.info} />
-			<AnimatePresence>
-				{isText && (
-					<motion.div
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						exit={{ opacity: 0 }}
-						transition={{ duration: 0.3 }}
-						className={classes.text}
-					>
-						<p>{text}</p>
-					</motion.div>
-				)}
-			</AnimatePresence>
+
+			<div className={classes.text}>
+				<p>{text}</p>
+			</div>
 		</motion.li>
 	);
 };
