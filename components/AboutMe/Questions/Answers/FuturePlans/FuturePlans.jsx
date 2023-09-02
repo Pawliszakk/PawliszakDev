9e7468da40d1701eae3f6181d8f9d34e7c500cf8
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-09-03 00:20:59 +0200

    added future plans

--- components/AboutMe/Questions/Answers/FuturePlans/FuturePlans.jsx
+++ components/AboutMe/Questions/Answers/FuturePlans/FuturePlans.jsx
@@ -1,3 +1,4 @@
+import AnswerHeading from '../../../../UI/Answers/AnswerHeading';
 import AnswerTemplate from '../../../../UI/Answers/AnswerTemplate';
 
 import classes from './FuturePlans.module.css';
@@ -5,15 +6,26 @@ import classes from './FuturePlans.module.css';
 const FuturePlans = () => {
 	return (
 		<AnswerTemplate heading="Future Plans">
-			<p>
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit. At aliquid
-				exercitationem sequi soluta dicta commodi vero voluptatibus sit
-				consectetur dolor ipsum sed, assumenda quasi voluptatem architecto
-				adipisci impedit laborum deleniti asperiores cumque officia ipsa maxime
-				nam alias? Impedit soluta ipsam facilis quod voluptatem sunt voluptate
-				necessitatibus, nam, dignissimos nesciunt cumque quisquam quam maiores
-				repudiandae, aliquam assumenda ducimus nisi illo ratione.
-			</p>
+			<div className={classes.box}>
+				<AnswerHeading className={classes.h3}>Mastering React.js</AnswerHeading>
+				<p className={classes.content}>
+					Continue to stay updated with the latest React.js features .Deepen
+					your expertise in these technology to build more complex and
+					interactive web applications.
+				</p>
+				<AnswerHeading>Stay Current with Web Standards:</AnswerHeading>
+				<p className={classes.content}>
+					Keep up with evolving web standards, browser capabilities, and best
+					practices to ensure your projects are compliant with the latest
+					technologies and recommendations.
+				</p>
+				<AnswerHeading>Continuous Learning</AnswerHeading>
+				<p className={classes.content}>
+					Frontend development is an ever-evolving field. Make continuous
+					learning a part of your career plan, whether through online courses,
+					workshops, conferences, or self-study.
+				</p>
+			</div>
 		</AnswerTemplate>
 	);
 };
